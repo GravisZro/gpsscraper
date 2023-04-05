e9d1c5df153a4e984c88e42af60d0a7ef10b7e99
GravisZro <noreply+github@adaptivetime.com>
2023-04-05 08:54:49 -0400

    utilities update
    add scoped enumeration operators
    move shortJSON helpers here for general use
    add serialization/deserialization wrapper

--- scrapers/utilities.cpp
+++ scrapers/utilities.cpp
@@ -171,3 +171,4 @@ namespace ext
     return offset;
   }
 }
+
--- scrapers/utilities.h
+++ scrapers/utilities.h
@@ -4,6 +4,13 @@
 #include <string>
 #include <set>
 #include <list>
+#include <stack>
+#include <vector>
+#include <type_traits>
+#include <optional>
+#include <cassert>
+
+#include <shortjson/shortjson.h>
 
 constexpr uint32_t operator "" _length(const char*, const size_t sz) noexcept
   { return sz; }
@@ -60,5 +67,152 @@ namespace ext
   };
 
 }
+
+// shortJSON helpers
+template<int line_number>
+inline std::optional<bool> safe_bool(shortjson::node_t& node)
+{
+  if(node.type == shortjson::Field::Boolean)
+    return node.toBool();
+  if(node.type == shortjson::Field::Null)
+    return std::optional<bool>();
+  throw line_number;
+}
+
+template<int line_number>
+inline std::optional<std::string> safe_string(shortjson::node_t& node)
+{
+  if(node.type == shortjson::Field::String)
+    return node.toString();
+  if(node.type == shortjson::Field::Null)
+    return std::optional<std::string>();
+  throw line_number;
+}
+
+template<int line_number, typename int_type = int32_t>
+inline std::optional<int_type> safe_int(shortjson::node_t& node)
+{
+  if(node.type == shortjson::Field::Integer)
+    return int_type(node.toNumber());
+  if(node.type == shortjson::Field::Null)
+    return std::optional<int_type>();
+  throw line_number;
+}
+
+template<int line_number>
+inline double safe_float64(shortjson::node_t& node)
+{
+  if(node.type == shortjson::Field::Integer)
+    return node.toNumber();
+  if(node.type == shortjson::Field::Float)
+    return node.toFloat();
+  throw line_number;
+}
+
+template<int line_number>
+inline float safe_float_unit(shortjson::node_t& node)
+{
+  if(node.type == shortjson::Field::String)
+    return std::stof(node.toString());
+  throw line_number;
+}
+
+template <class container, std::enable_if_t<std::is_arithmetic_v<typename container::value_type>, bool> = true>
+struct serializable : container
+{
+  using T = typename container::value_type;
+  using container::container;
+  using container::operator=;
+
+  operator std::vector<uint8_t>(void) const
+  {
+    std::vector<uint8_t> output;
+    for(T element : *this)
+      for(size_t i = 0; i < sizeof(T); ++i)
+        output.push_back(reinterpret_cast<uint8_t*>(&element)[i]);
+    return output;
+  }
+
+  serializable<container>& operator =(const std::vector<uint8_t>& input)
+  {
+    for(auto pos = std::begin(input); pos != std::end(input); std::advance(pos, sizeof(T)))
+    {
+      T element = *pos;
+      for(size_t i = 1; i < sizeof(T); ++i)
+        reinterpret_cast<uint8_t*>(&element)[i] = *std::next(pos, i);
+      container::push_back(element);
+    }
+    return *this;
+  }
+
+};
+
+// polyfill for is_scoped_enum
+#if !defined(__cpp_lib_is_scoped_enum) || __cpp_lib_is_scoped_enum < 202011L
+#define __cpp_lib_is_scoped_enum 202011L
+namespace std
+{
+  namespace detail {
+  namespace { // avoid ODR-violation
+  template<class T>
+  auto test_sizable(int) -> decltype(sizeof(T), std::true_type{});
+  template<class>
+  auto test_sizable(...) -> std::false_type;
+
+  template<class T>
+  auto test_nonconvertible_to_int(int)
+      -> decltype(static_cast<std::false_type (*)(int)>(nullptr)(std::declval<T>()));
+  template<class>
+  auto test_nonconvertible_to_int(...) -> std::true_type;
+
+  template<class T>
+  constexpr bool is_scoped_enum_impl = std::conjunction_v<
+      decltype(test_sizable<T>(0)),
+      decltype(test_nonconvertible_to_int<T>(0))
+  >;
+  }
+  } // namespace detail
+
+
+  template<typename E>
+  struct is_scoped_enum : std::bool_constant<detail::is_scoped_enum_impl<E>> {};
+
+  template< class T >
+  inline constexpr bool is_scoped_enum_v = is_scoped_enum<T>::value;
+}
+#endif
+
+
+// add operations for scoped enumberations
+/*
+template<typename enum_type, std::enable_if_t<std::is_scoped_enum_v<enum_type>, bool> = true>
+constexpr bool operator ==(enum_type a, enum_type b)
+  { return std::underlying_type_t<enum_type>(a) == std::underlying_type_t<enum_type>(b); }
+*/
+
+
+template<typename enum_type, std::enable_if_t<std::is_scoped_enum_v<enum_type>, bool> = true>
+constexpr std::underlying_type_t<enum_type> operator |=(enum_type a, typename std::underlying_type_t<enum_type> b)
+  { return static_cast<typename std::underlying_type_t<enum_type>>(a) | b; }
+
+template<typename enum_type, std::enable_if_t<std::is_scoped_enum_v<enum_type>, bool> = true>
+constexpr std::underlying_type_t<enum_type> operator |=(enum_type a, enum_type b)
+  { return static_cast<typename std::underlying_type_t<enum_type>>(a) | static_cast<typename std::underlying_type_t<const enum_type>>(b); }
+
+template<typename enum_type, std::enable_if_t<std::is_scoped_enum_v<enum_type>, bool> = true>
+constexpr std::underlying_type_t<enum_type> operator |(enum_type a, typename std::underlying_type_t<enum_type> b)
+  { return static_cast<typename std::underlying_type_t<enum_type>>(a) | b; }
+
+template<typename enum_type, std::enable_if_t<std::is_scoped_enum_v<enum_type>, bool> = true>
+constexpr enum_type operator |(enum_type a, enum_type b)
+  { return static_cast<enum_type>(
+            static_cast<typename std::underlying_type_t<enum_type>>(a) |
+            static_cast<typename std::underlying_type_t<enum_type>>(b)); }
+
+
+template<typename enum_type, std::enable_if_t<std::is_scoped_enum_v<enum_type>, bool> = true>
+constexpr std::underlying_type_t<enum_type> operator &(enum_type a, enum_type b)
+  { return static_cast<typename std::underlying_type_t<enum_type>>(a) &
+           static_cast<typename std::underlying_type_t<enum_type>>(b); }
 
 #endif // UTILITIES_H
